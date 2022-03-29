54b2cb8c8abe004d1af191a6a4d77d2683371566
Bendali Safarli <ccsafarli@gmail.com>
2022-03-29 23:49:44 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.status(200).send('Ana sehifeye xos geldiniz!')
+})
+
+app.get('/books', (req, res) => {
+	res.status(200).send('Burada siz istediyinz kitablari tapa bilersiniz')
+})
+
+app.listen('8080', () => console.log('SERVER STARTED LISTENING ON PORT: 8080'))
